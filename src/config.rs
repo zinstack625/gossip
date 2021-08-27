277c6b8ef197ec56fe850e08d4bbcdde9ba96f6a
zinstack625 <klimanov625@outlook.com>
2021-08-27 11:51:37 +0000

    gossip: elementary configuration

--- src/config.rs
+++ src/config.rs
@@ -2,3 +2,11 @@ pub struct Config {
     pub max_send_peers: usize,
     pub stored_messages_filename: String,
 }
+impl Config {
+    pub fn new() -> Config {
+        Config {
+            max_send_peers: 5,
+            stored_messages_filename: String::from(""),
+        }
+    }
+}
